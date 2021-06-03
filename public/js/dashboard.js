6649c9e28ffcb1444cc31bcf2ec7267183360bd6
Michelle Watts <watts.e.michelle@gmail.com>
2021-06-03 12:31:17 +1000

    updating

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -40,37 +40,6 @@ const delButtonHandler = async (event) => {
   }
 };
 
-async function editFormHandler(event) {
-  event.preventDefault();
-
-  const title = document.querySelector('input[name="post-title"]').value.trim();
-  const content = document
-    .querySelector('textarea[name="content"]')
-    .value.trim();
-
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-  ];
-
-  const response = await fetch(`/api/posts/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-      post_id: id,
-      title,
-      content,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  if (response.ok) {
-    document.location.replace('/dashboard/');
-  } else {
-    alert(response.statusText);
-  }
-}
-
 document
   .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
@@ -78,7 +47,3 @@ document
 document
   .querySelector('.delete-post-btn')
   .addEventListener('click', delButtonHandler);
-
-document
-  .querySelector('.edit-post-btn')
-  .addEventListener('click', editFormHandler);
