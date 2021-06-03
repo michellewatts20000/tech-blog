ee793f95cad584237c20a9808b472153191cd275
Michelle Watts <watts.e.michelle@gmail.com>
2021-06-03 12:41:58 +1000

    working on the put request

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -36,4 +36,29 @@ router.delete('/:id', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+      content: req.body.content,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
