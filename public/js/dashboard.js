836ec04110c8b2d73d34947dec8b4ea8f0247de2
Michelle Watts <watts.e.michelle@gmail.com>
2021-06-03 11:21:08 +1000

    update post

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -40,29 +40,43 @@ const delButtonHandler = async (event) => {
   }
 };
 
-// const upButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-id')) {
-//     const id = event.target.getAttribute('data-id');
-//     const response = await fetch(`/api/post/${id}`, {
-//       method: 'UPDATE',
-//     });
+async function editFormHandler(event) {
+  event.preventDefault();
 
-//     if (response.ok) {
-//       document.location.replace('/dashboard');
-//     } else {
-//       alert('Failed to update post');
-//     }
-//   }
-// };
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const content = document.querySelector('input[name="content"]').value.trim();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      post_id: id,
+      title,
+      content,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+}
 
 document
   .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.post-list')
+  .querySelector('.delete-post-btn')
   .addEventListener('click', delButtonHandler);
 
-// document
-//   .querySelector('.post-update')
-//   .addEventListener('click', upButtonHandler);
+document
+  .querySelector('.edit-post-form')
+  .addEventListener('click', editFormHandler);
