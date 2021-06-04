21d604e80f951205532f4a5d4c577d4db9cc3545
Your NameMichelle <watts.e.michelle@gmail.com>
2021-06-04 20:13:13 +1000

    helper function to truncate text

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,20 +3,14 @@ module.exports = {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
-  get_emoji: () => {
-    const randomNum = Math.random();
 
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
-    }
+  truncate_text: (description) => {
+    var length = 300;
+    return description.substring(0, length) + '...';
+  },
+
+  shorter_extract: (description) => {
+    var length = 200;
+    return description.substring(0, length) + '...';
   },
 };
